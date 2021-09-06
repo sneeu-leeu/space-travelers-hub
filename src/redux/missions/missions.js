bd6cfcc4ccebc48b0ab081839df91689045ec137
sneeu-leeu <sa070197@gmail.com>
2021-09-06 22:12:47 +0200

    create redux mission add

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -2,15 +2,21 @@ const apiEndpoint = 'https://api.spacexdata.com/v3/missions';
 const initialState = [];
 
 const ADD_MISSION = 'rockets/ADD_MISSION';
+const JOIN_MISSION = 'missions/JOIN_MISSION';
 
 const fetchAllMissions = async () => {
-  let res = await fetch(apiEndpoint);
-  res = await res.json();
-  return res;
+  let result = await fetch(apiEndpoint);
+  result = await result.json();
+  return result;
 };
 
 const addMission = (mission) => ({
   type: ADD_MISSION,
+  payload: mission,
+});
+
+const joinMission = (mission) => ({
+  type: JOIN_MISSION,
   payload: mission,
 });
 
@@ -28,9 +34,23 @@ export const fetchMissions = async (dispatch, getState) => {
 };
 
 const reducer = (state = initialState, action) => {
+  let newState;
+  let theMission;
   switch (action.type) {
-    case ADD_MISSION:
-      return [...state, action.payload];
+    case JOIN_MISSION:
+      theMission = state.find((mission) => mission.mission_id === action.payload.mission_id);
+      if (theMission && theMission.reserved) {
+        newState = state.map((mission) => {
+          if (mission.mission_id !== action.payload.mission_id) return mission;
+          return { ...mission, reserved: false };
+        });
+        return newState;
+      }
+      newState = state.map((mission) => {
+        if (mission.mission_id !== action.payload.mission_id) return mission;
+        return { ...mission, reserved: true };
+      });
+      return newState;
     default:
       return state;
   }
