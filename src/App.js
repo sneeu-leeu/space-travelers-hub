3ad5c702ec4d28a96d7c35566f73e245255d63c3
sneeu-leeu <sa070197@gmail.com>
2021-09-06 17:12:08 +0200

    Add store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
@@ -8,20 +10,22 @@ import Profile from './components/Profile';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/rockets">
-          <Rockets />
-        </Route>
-        <Route path="/missions">
-          <Missions />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Rockets />
+          </Route>
+          <Route path="/missions">
+            <Missions />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
