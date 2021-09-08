aab7e205f1f4e34c65455f9a5312e74b981a95c3
sneeu-leeu <sa070197@gmail.com>
2021-09-08 22:44:23 +0200

    Style and even out profile.js

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,17 +10,39 @@ import {
   Button,
 } from 'react-bootstrap';
 import { cancelReservation } from '../redux/rockets/rockets';
-import { missions } from '../redux/missions/missions';
+import { missions, toggleMissionState } from '../redux/missions/missions';
 
 const Profile = () => {
   const allMissions = useSelector(missions);
+  const dispatch = useDispatch();
+
+  const leaveMission = (e) => {
+    dispatch(toggleMissionState({ mission_id: e.target.id }));
+  };
+
   const reservedMissions = allMissions.filter((mission) => mission.reserved).map(
     (mission) => (
-      <ListGroupItem className="pb-4" key={mission.mission_id}>{mission.mission_name}</ListGroupItem>
+      <ListGroupItem key={mission.mission_id} className="d-flex align-items-center justify-content-between">
+        <span>
+          {mission.mission_name}
+          <br />
+          <a href={mission.wikipedia} target="blank">Read more</a>
+        </span>
     ),
   );
   const rocketState = useSelector((state) => state.rocketsReducer);
-  const dispatch = useDispatch();
+  const reservedRockets = rocketState.filter((rocket) => rocket.reserved).length === 0
+    ? <ListGroupItem>No rockets reserved </ListGroupItem>
+    : rocketState.filter((rocket) => rocket.reserved).map((rocket) => (
+      <ListGroupItem key={rocket.id} className="d-flex align-items-center justify-content-between">
+        <div>
+          {rocket.rocket_name}
+          <br />
+          <a href={rocket.wikipedia} target="blank">Read More</a>
+        </div>
+
+      </ListGroupItem>
+    ));
   return (
     <Container fluid className="border-top w-100 pt-2">
       <Row>
@@ -28,23 +50,21 @@ const Profile = () => {
           <h2>My Missions</h2>
           <Card>
             <ListGroup>
-              {reservedMissions}
+              {reservedMissions.length > 0 ? reservedMissions : (
+                <ListGroupItem>
+                  No Missions Joined
+                </ListGroupItem>
+              )}
             </ListGroup>
           </Card>
         </Col>
         <Col xs={12} md={6}>
           <h2>My Rockets</h2>
-          <ListGroup>
-            {rocketState.filter((rocket) => rocket.reserved).length === 0
-              ? <ListGroup.Item>No rockets reserved </ListGroup.Item>
-              : rocketState.filter((rocket) => rocket.reserved).map((rocket) => (
-                <ListGroup.Item key={rocket.id} className="list-group-item d-flex justify-content-between">
-                  {rocket.rocket_name}
-                  {' '}
-                  <Button variant="danger" onClick={() => dispatch(cancelReservation(rocket.id))}>Cancel Reservation</Button>
-                </ListGroup.Item>
-              ))}
-          </ListGroup>
+          <Card>
+            <ListGroup>
+              {reservedRockets}
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </Container>
