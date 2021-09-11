7b2b07040ab0e80014afd3def12b4389bfb13564
uwadonat <uwadonat@gmail.com>
2021-09-12 00:23:59 +0300

    Fix eslint

--- src/__test__/Rockets.test.js
+++ src/__test__/Rockets.test.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
 import Rockets from '../components/Rockets';
 import store from '../redux/configureStore';
-import { Provider } from 'react-redux';
-
 
 describe('check if all component are rendered', () => {
-    
-    it('renders correctly ', () => {
-       const tree = renderer.create(<Provider store={store}><Rockets /></Provider>).toJSON();
-       expect(tree).toMatchSnapshot();
-    });
+  it('renders correctly ', () => {
+    const tree = renderer.create(<Provider store={store}><Rockets /></Provider>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
-
