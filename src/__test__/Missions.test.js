c8cc05decd2e8508656186d38d24b6ae43ee3a43
uwadonat <uwadonat@gmail.com>
2021-09-12 00:17:17 +0300

    Add test

---
+++ src/__test__/Missions.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Missions from '../components/Missions';
+import store from '../redux/configureStore';
+import { Provider } from 'react-redux';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Space travelers hub app', () => {
+    test('renders Missions', () => {
+        render(<Provider store={store}><BrowserRouter><Missions /></BrowserRouter></Provider>);
+        expect(screen.getByText('Mission')).toBeInTheDocument();
+    });
+});
---
+++ src/__test__/Rockets.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import renderer from 'react-test-renderer';
+import Rockets from '../components/Rockets';
+import store from '../redux/configureStore';
+import { Provider } from 'react-redux';
+
+
+describe('check if all component are rendered', () => {
+    
+    it('renders correctly ', () => {
+       const tree = renderer.create(<Provider store={store}><Rockets /></Provider>).toJSON();
+       expect(tree).toMatchSnapshot();
+    });
+});
+
